83d72326786bfe8a1552e0dfde366331ef79095a
AlexanderByndyu <alexander.byndyu@gmail.com>
2016-01-26 17:42:08 +0500

    Init dotnet project

---
+++ Projects/dotnet/source/AgileCamp.Habitator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AgileCamp.Habitator.Startup))]
+namespace AgileCamp.Habitator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
